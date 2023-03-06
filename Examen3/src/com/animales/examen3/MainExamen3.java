77566a27c9e572c6e4f4bf090cb6471906215b2d
Veronica Ceban <123936150+Veronicact1@users.noreply.github.com>
2023-03-06 18:23:59 +0100

    c

--- Examen3/src/com/animales/examen3/MainExamen3.java
+++ Examen3/src/com/animales/examen3/MainExamen3.java
@@ -4,6 +4,9 @@ public class MainExamen3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Delfin White_delfin = new Delfin();
+	Leopardo Hembra = new Leopardo();
+	Colibri greenBlue = new Colibri();
 
 	}
 
