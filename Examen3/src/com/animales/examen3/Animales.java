6142c06949913d904dec417dff9476f8bd0d4bbe
Veronica Ceban <123936150+Veronicact1@users.noreply.github.com>
2023-03-06 18:09:20 +0100

    c

--- Examen3/src/com/animales/examen3/Animales.java
+++ Examen3/src/com/animales/examen3/Animales.java
@@ -1,21 +1,18 @@
 package com.animales.examen3;
 
 public class Animales implements SeresVivos {
-
-	public static void main(String[] args) {
 		// Atribjuto
 		public int velocidad;
 		// Metodo
-		public void reactionar() {}
+		public abstract void reactionar() {}
 		//constructors
 		
 		//getter&setter
-		
-		
-		
-	}
 
 }
+
+
+
 //Crea un aplicativo en
 //“MainExamen3.java” creando 3 objetos de cada clase que muestre, mediante los
 //métodos de cada clase Colibri, Delfin y Leopardo, por pantalla sus atributos, es
