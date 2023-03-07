bfaf2143234000ee5e2cf2d972c18828c017663a
Veronica Ceban <123936150+Veronicact1@users.noreply.github.com>
2023-03-07 14:45:57 +0100

    v

--- SQL/src/PruebaConection.java
+++ SQL/src/PruebaConection.java
@@ -2,7 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class PruebaConection {{
+public class PruebaConection {
+	public static void main(String[] args){
 	try {
 		/*
 		subprotocolo: mysql 
